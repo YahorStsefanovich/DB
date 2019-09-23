d304ddf32fd5b52ca0734f88bbc8daf473000a8d
Yahor Stsefanovich <yegorstsephanovich@gmail.com>
2019-09-24 00:19:37 +0300

    Fixed lab 6

--- lab6/V4_1.sql
+++ lab6/V4_1.sql
@@ -15,24 +15,43 @@ CREATE PROCEDURE dbo.NumberOfEmployeesByYear @listOfYears varchar(MAX)
 AS
 BEGIN
     DECLARE @query AS nvarchar(MAX);
-    set @query =
-                'SELECT Name, ' + @listOfYears + '
-		FROM (
-				SELECT D.Name,
-				YEAR (ED.StartDate) as YearOfStartDate,
-				D.DepartmentID
-				FROM HumanResources.Department AS D
-				JOIN HumanResources.EmployeeDepartmentHistory AS ED
-				ON D.DepartmentID = ED.DepartmentID
-			)
-		AS DepartmentEmployees
-		PIVOT (
-			COUNT (DepartmentID)
-			FOR YearOfStartDate IN (' + @listOfYears + ')
-		) AS CountOfEmployees
-		ORDER BY Name;'
-    execute
-        (@ query)
+	DECLARE @itemYear INT, 
+			@endYear INT;
+
+	IF OBJECT_ID('tempdb.dbo.#everyYearEmployeesHistory', 'U') IS NOT NULL
+		DROP TABLE #everyYearEmployeesHistory;
+
+	CREATE TABLE #everyYearEmployeesHistory (currentYear INT, departmentID INT, name NVARCHAR(MAX));
+		
+	SET @itemYear = SUBSTRING(@listOfYears, 2, 4);
+	SET @endYear = SUBSTRING(@listOfYears, LEN(@listOfYears) - 4, 4);
+
+	WHILE (@itemYear <= @endYear)
+	BEGIN
+		INSERT INTO #everyYearEmployeesHistory
+		SELECT  @itemYear,
+				D.DepartmentID,
+				D.Name
+		FROM HumanResources.Department AS D
+		JOIN HumanResources.EmployeeDepartmentHistory AS ED
+		ON D.DepartmentID = ED.DepartmentID 
+		-- if endDate null then get 2019;
+		-- minus 1: if employee was sacked in currentYear, then we don't count him) 
+		WHERE @itemYear BETWEEN YEAR(ED.StartDate) AND ((ISNULL(YEAR(ED.EndDate), YEAR(GETDATE()))) - 1)
+		SET @itemYear = @itemYear + 1; 
+	END;
+
+    set @query ='
+					SELECT Name,' + @listOfYears + ' 
+					FROM #everyYearEmployeesHistory
+					AS DepartmentEmployees
+					PIVOT (
+						COUNT (DepartmentID)
+						FOR currentYear IN (' + @listOfYears + ')
+					) AS CountOfEmployees
+					ORDER BY Name;
+				'
+    execute(@query);
 END;
 GO
 

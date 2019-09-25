f0ee468883e0f67f35c8262886d4f82fe324882c
Yahor Stsefanovich <yegorstsephanovich@gmail.com>
2019-09-25 15:21:33 +0300

    Fixed lab 7

--- lab7/V4_1.sql
+++ lab7/V4_1.sql
@@ -10,18 +10,40 @@ GO
 CREATE PROCEDURE [dbo].vendorToXML @xmlForVendors XML
 AS
 BEGIN
+    IF object_id('tempdb.#resultTable') IS NULL
+        BEGIN
+            CREATE TABLE #resultTable
+            (
+                [BusinessEntityID] int,
+                [Name]             nvarchar(50),
+                [AccountNumber]    nvarchar(15)
+            )
+        END;
+
+    INSERT #resultTable
     SELECT [BusinessEntityID] = Node.Data.value('(ID)[1]', 'INT')
          , [Name]             = Node.Data.value('(Name)[1]', 'NAME')
          , [AccountNumber]    = Node.Data.value('(AccountNumber)[1]', 'AccountNumber')
     FROM @xmlForVendors.nodes('/Vendors/Vendor') Node(Data)
+
+    SELECT * FROM #resultTable;
 END;
 
-DECLARE @vendorsFromXML XML;
-SELECT @vendorsFromXML = (
-    SELECT [BusinessEntityID] AS ID, [Name], [AccountNumber]
-    FROM [Purchasing].[Vendor]
-    FOR XML RAW ('Vendor'), TYPE, ELEMENTS, ROOT ('Vendors')
-);
-
-    EXEC [dbo].vendorToXML @vendorsFromXML;
+    IF OBJECT_ID('[dbo].getResultTable', 'P') IS NOT NULL
+        DROP PROCEDURE getResultTable;
 GO
+
+CREATE PROCEDURE [dbo].getResultTable
+AS
+BEGIN
+    DECLARE @vendorsFromXML XML;
+    SELECT @vendorsFromXML = (
+        SELECT [BusinessEntityID] AS ID, [Name], [AccountNumber]
+        FROM [Purchasing].[Vendor]
+        FOR XML RAW ('Vendor'), TYPE, ELEMENTS, ROOT ('Vendors')
+    );
+    EXEC [dbo].vendorToXML @vendorsFromXML;
+END;
+GO
+
+EXEC getResultTable;

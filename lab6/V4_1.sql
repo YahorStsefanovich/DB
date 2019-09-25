9d853a3aa41ccb6d48aa8967dabd0d8d00c38914
Yahor Stsefanovich <yegorstsephanovich@gmail.com>
2019-09-25 13:06:03 +0300

    Fixed lab 6 via CTE

--- lab6/V4_1.sql
+++ lab6/V4_1.sql
@@ -6,7 +6,6 @@
 Таким образом, вызов процедуры будет выглядеть следующим образом:
 
 EXECUTE dbo.EmpCountByDep ‘[2003],[2004],[2005]’*/
-
 IF OBJECT_ID('dbo.NumberOfEmployeesByYear', 'P') IS NOT NULL
     DROP PROCEDURE dbo.NumberOfEmployeesByYear;
 GO
@@ -15,47 +14,46 @@ CREATE PROCEDURE dbo.NumberOfEmployeesByYear @listOfYears varchar(MAX)
 AS
 BEGIN
     DECLARE @query AS nvarchar(MAX);
-    DECLARE @itemYear INT,
-        @endYear INT;
+    set @query =
+        '
+            DECLARE @itemYear INT,
+                @endYear INT;
 
-    IF OBJECT_ID('tempdb.dbo.#everyYearEmployeesHistory', 'U') IS NOT NULL
-        DROP TABLE #everyYearEmployeesHistory;
-
-    CREATE TABLE #everyYearEmployeesHistory
-    (
-        currentYear  INT,
-        departmentID INT,
-        name         NVARCHAR(MAX)
-    );
-
-    SET @itemYear = SUBSTRING(@listOfYears, 2, 4);
-    SET @endYear = SUBSTRING(@listOfYears, LEN(@listOfYears) - 4, 4);
-
-    WHILE (@itemYear <= @endYear)
-        BEGIN
-            INSERT INTO #everyYearEmployeesHistory
-            SELECT @itemYear,
-                   D.DepartmentID,
-                   D.Name
-            FROM HumanResources.Department AS D
-                     JOIN HumanResources.EmployeeDepartmentHistory AS ED
-                          ON D.DepartmentID = ED.DepartmentID
-                 -- if endDate null then get 2019;
-                 -- minus 1: if employee was sacked in currentYear, then we don't count him)
-            WHERE @itemYear BETWEEN YEAR(ED.StartDate) AND ((ISNULL(YEAR(ED.EndDate), YEAR(GETDATE()))) - 1)
-            SET @itemYear = @itemYear + 1;
-        END;
-
-    set @query = '
-					SELECT Name,' + @listOfYears + ' 
-					FROM #everyYearEmployeesHistory
-					AS DepartmentEmployees
-					PIVOT (
-						COUNT (DepartmentID)
-						FOR currentYear IN (' + @listOfYears + ')
-					) AS CountOfEmployees
-					ORDER BY Name;
-				'
+            SET @itemYear = ' + SUBSTRING(@listOfYears, 2, 4) + ';
+			SET @endYear = ' + SUBSTRING(@listOfYears, LEN(@listOfYears) - 4, 4) + ';
+			WITH unitedDepartmentHistTable(currentYear, DepartmentID, Name, StartYear, EndYear)
+			AS 
+			(
+				SELECT @itemYear,
+					D.DepartmentID,
+					D.Name,
+					YEAR(ED.StartDate) as StartYear,
+					ISNULL(YEAR(ED.EndDate), YEAR(GETDATE())) as EndYear
+				FROM HumanResources.Department AS D
+						JOIN HumanResources.EmployeeDepartmentHistory AS ED
+							ON D.DepartmentID = ED.DepartmentID
+				UNION ALL
+				SELECT currentYear+1,
+						DepartmentID,
+						Name,
+						StartYear,
+						EndYear
+				FROM unitedDepartmentHistTable
+				WHERE currentYear < @endYear 
+			)
+			SELECT Name,' + @listOfYears + ' 
+			FROM (
+				SELECT name, currentYear, DepartmentID
+				FROM unitedDepartmentHistTable 
+				WHERE currentYear between StartYear AND EndYear - 1
+			)
+			AS DepartmentEmployees
+			PIVOT (
+				COUNT (DepartmentID)
+				FOR currentYear IN (' + @listOfYears + ')
+			) AS CountOfEmployees
+			ORDER BY Name;
+		'
     execute (@query);
 END;
 GO
